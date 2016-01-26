6abb484699140774d10428f489b18dc5a9fbae9a
Alvaro Molina <alvaromolinac@gmail.com>
2016-01-26 19:15:17 +0100

    prueba

--- app/grids/company_reports_grid.rb
+++ app/grids/company_reports_grid.rb
@@ -3,7 +3,7 @@ class CompanyReportsGrid
   include Datagrid
 
   scope do
-    Company
+    Company.where('(select count(1) from contracted_companies where contracted_companies.company_id = companies.id) > 0')
   end
 
   filter(:name, :string, :header => "Nombre empresa:") { |value| where("name ilike '%#{value}%'") }
